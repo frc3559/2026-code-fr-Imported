4e4dc5111b7d503c311ae48e8be1357235a19a9f
Jasper Robotics <jhsrobotics@gjcs.k12.in.us>
2026-02-17 18:31:52 -0500

    Shooter save issue fixed

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,20 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterSubsystem extends SubsystemBase{
-    
+public class ShooterSubsystem  extends SubsystemBase{
+    private final SparkFlex shooterMotor = new SparkFlex(21, MotorType.kBrushless);
+
+    public ShooterSubsystem(){
+
+    }
+
+    public void shooterSet(double speed){
+    shooterMotor.set(speed);
+    }
+
+
 }
