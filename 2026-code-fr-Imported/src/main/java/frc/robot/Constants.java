ef41abe26f48be051d7d23ce533c2189076e57e1
Emmett <emmetthurm@gmail.com>
2026-04-21 19:18:17 -0400

    fixed controller issue

--- 2026-code-fr-Imported/src/main/java/frc/robot/Constants.java
+++ 2026-code-fr-Imported/src/main/java/frc/robot/Constants.java
@@ -79,7 +79,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
-    public static final int kOperatorControllerPort = 3;
+    public static final int kOperatorControllerPort = 1;
     //increased deadband from 0.05
     public static final double kDriveDeadband = 0.10;
     public static final double kTeleopMaxSpeedMPS = 3;
